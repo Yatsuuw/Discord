44c7981a3713e627e6c781253481df9d62115a9d
Yatsuu <pieters.lucas87@gmail.com>
2026-04-30 23:56:55 +0200

    Modification de l'import Prisma

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "../generated/prisma/index.js";
 import { logger } from "./logger.js";
 
 export const db = new PrismaClient({} as ConstructorParameters<typeof PrismaClient>[0]);
