7c47958ded2aa24a028afe20de34278219e20bef
Yatsuu <pieters.lucas87@gmail.com>
2026-04-30 23:26:25 +0200

    Ajout de la commande pour initialiser les serveurs dans la base de données

---
+++ src/commands/admin/init.ts
@@ -0,0 +1,45 @@
+import { ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { DataManager } from "../../utils/dataManager.js";
+import { Templates } from "../../utils/templates.js";
+import { db } from "../../utils/database.js";
+
+export const command = {
+  data: new SlashCommandBuilder()
+    .setName('init')
+    .setDescription('Initialise le serveur dans la base de données')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (!interaction.guild) return;
+
+    const guildId = interaction.guild.id;
+
+    try {
+      const existingServer = await db.servers.findUnique({
+        where: {
+          id_owner: {
+            id: guildId,
+            owner: interaction.guild.ownerId
+          }
+        }
+      });
+
+      if (existingServer) {
+        return interaction.reply({
+          embeds: [Templates.info('Information `/init` - Administration', [ { name: 'Retour de la base de données :', value: `Ce serveur est déjà enregistré dans la base de données.` } ])],
+          ephemeral: true
+        });
+      }
+
+      await DataManager.registerServer(guildId, interaction.guild.ownerId);
+
+      return interaction.reply({
+        embeds: [Templates.success(`Le serveur **${interaction.guild.name}** a été initialisé dans la base de données avec succès.`)]
+      });
+    } catch (error) {
+      return interaction.reply({
+        embeds: [Templates.error(`Erreur lors de l'initialisation du serveur : ${error}`)],
+        ephemeral: true
+      });
+    }
+  }
+};
