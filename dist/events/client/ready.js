6d2aca3a9807eb4b75422682cfa9bcfc195ae51e
Yatsuu <pieters.lucas87@gmail.com>
2026-05-01 01:51:29 +0200

    Compilation de la dernière modification du console log en logger.

--- dist/events/client/ready.js
+++ dist/events/client/ready.js
@@ -1,9 +1,10 @@
 import { Events } from "discord.js";
+import { logger } from "../../utils/logger.js";
 const event = {
     name: Events.ClientReady,
     once: true,
     execute(_client, c) {
-        console.log(`${c.user.tag} est connecté.`);
+        logger.info(`${c.user.tag} est connecté.`);
     }
 };
 export default event;
