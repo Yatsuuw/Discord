1621ad888b64298c4076b02cff002888238dcec1
Yatsuu <pieters.lucas87@gmail.com>
2026-04-30 23:57:30 +0200

    Compilation du changement d'import de Prisma dans database

--- dist/utils/database.d.ts
+++ dist/utils/database.d.ts
@@ -1,3 +1,4 @@
-export declare const db: import("../generated/prisma/internal/class.js").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
+import { PrismaClient } from "../generated/prisma/index.js";
+export declare const db: PrismaClient<import("../generated/prisma/index.js").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/client.js").DefaultArgs>;
 export declare function connectDB(): Promise<void>;
 //# sourceMappingURL=database.d.ts.map
