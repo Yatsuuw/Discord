5b143b86de1d082273a325f3c4cded1e6170dea6
Yatsuu <pieters.lucas87@gmail.com>
2026-05-01 02:07:21 +0200

    Ajout de la déconnexion de la base de données et de la sortie automatique lors d'un unhandledRejection et uncaughtException

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { GatewayIntentBits } from 'discord.js';
 import { ExtendedClient } from './structures/ExtendedClient.js';
 import { logger } from './utils/logger.js';
-import { connectDB } from './utils/database.js';
+import { connectDB, db } from './utils/database.js';
 
 const client = new ExtendedClient({
   intents: [
@@ -16,11 +16,19 @@ const client = new ExtendedClient({
   await client.start();
 })();
 
-process.on('unhandledRejection', (reason) => {
+process.on('unhandledRejection', async (reason) => {
   logger.error('Promesse non gérée détectée (Unhandled Rejection) :', reason);
+
+  await db.$disconnect();
+  await new Promise(resolve => setTimeout(resolve, 50));
+  process.exit(1);
 });
 
-process.on('uncaughtException', (err, origin) => {
+process.on('uncaughtException', async (err, origin) => {
   logger.error('Exception non capturée (Uncaught Exception) :', err);
   logger.error(`Origine de l'erreur : ${origin}`);
+
+  await db.$disconnect();
+  await new Promise(resolve => setTimeout(resolve, 50));
+  process.exit(1);
 });
