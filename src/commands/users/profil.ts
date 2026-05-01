750476501177c73d2e3b855031dade115637b66d
Yatsuu <pieters.lucas87@gmail.com>
2026-05-01 03:02:25 +0200

    Modification du message de réponse avec le lien

--- src/commands/users/profil.ts
+++ src/commands/users/profil.ts
@@ -72,7 +72,7 @@ export const command = {
       }
 
       const embed = Templates.info(`${siteName} - ${target.username}`, [
-        { name: 'Lien du profil', value: `[${profileName}](${url})` }
+        { name: 'Lien du profil', value: `[Lien vers la page ${siteName}](${url})` }
       ]);
 
       embed.setThumbnail(target.displayAvatarURL({ size: 512 }));
