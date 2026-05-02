b14a00f84c1976120e9fededa8f6533561646226
Yatsuu <pieters.lucas87@gmail.com>
2026-05-02 03:21:28 +0200

    Suppression des ! pour éviter la confusion et des anti-pattern setTimeout et ajout d'un exitCode dans le catch

--- src/deploy.ts
+++ src/deploy.ts
@@ -4,38 +4,30 @@ import { config } from "./config.js";
 import { db } from "./utils/database.js";
 import { logger } from "./utils/logger.js";
 
-const client = new ExtendedClient({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
-});
-
 const deploy = async () => {
   const isGlobal = process.argv.includes('--global');
 
+  const client = new ExtendedClient({ intents: [GatewayIntentBits.Guilds] });
   await client.loadCommands();
 
-  const rest = new REST({ version: '10' }).setToken(config.token!);
+  const rest = new REST({ version: '10' }).setToken(config.token);
   const commandData = client.commands.map(cmd => cmd.data.toJSON());
 
   try {
     logger.info(`Déploiement ${isGlobal ? 'global' : 'local'} des commandes...`);
 
     const route = isGlobal
-      ? Routes.applicationCommands(config.clientId!)
-      : Routes.applicationGuildCommands(config.clientId!, config.guildId!);
+      ? Routes.applicationCommands(config.clientId)
+      : Routes.applicationGuildCommands(config.clientId, config.guildId);
 
     await rest.put(route, { body: commandData });
 
     logger.info(`Commandes ${isGlobal ? 'globales' : 'locales'} déployées avec succès.`);
-    await db.$disconnect();
-    await new Promise(resolve => setTimeout(resolve, 50));
-    process.exit(0);
   } catch (error) {
     logger.error('Erreur lors du déploiement des commandes :', error);
+    process.exitCode = 1;
+  } finally {
     await db.$disconnect();
-    await new Promise(resolve => setTimeout(resolve, 50));
-    process.exit(1);
   }
 };
 
