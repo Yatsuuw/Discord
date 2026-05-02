fe18537320a05e7f48b9a598a068cd6cf027bbed
Yatsuu <pieters.lucas87@gmail.com>
2026-05-02 02:11:59 +0200

    Correction d'une faute de frappe

--- src/events/interactions/interactionCreate.ts
+++ src/events/interactions/interactionCreate.ts
@@ -12,7 +12,7 @@ const event: Event<Events.InteractionCreate> = {
     const command = client.commands.get(interaction.commandName);
 
     if (!command) {
-      logger.warn(`Commande inconnue invoquée : /${interaction.commandName} âr ${interaction.user.tag}`);
+      logger.warn(`Commande inconnue invoquée : /${interaction.commandName} par ${interaction.user.tag}`);
       await interaction.reply({
         embeds: [Templates.error('Cette commande n\'existe pas ou a été désactivée.')],
         flags: MessageFlags.Ephemeral
