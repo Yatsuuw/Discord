2aad01c402ded06c413b7ceac5972ab5b9e07182
Yatsuu <pieters.lucas87@gmail.com>
2026-05-02 03:22:21 +0200

    Modification du .ts en .js dans l'import

--- src/types/discord.d.ts
+++ src/types/discord.d.ts
@@ -1,5 +1,5 @@
 import { Collection } from 'discord.js';
-import type { Command } from './index.ts';
+import type { Command } from './index.js';
 
 declare module 'discord.js' {
   export interface Client {
