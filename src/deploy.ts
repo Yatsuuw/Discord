900b2418df20f134517e750e2bd9d53ce1aa15ce
Yatsuu <pieters.lucas87@gmail.com>
2026-05-02 03:26:33 +0200

    Retrait de l'intent inutile pour le déploiement

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,4 +1,4 @@
-import { GatewayIntentBits, REST, Routes } from "discord.js";
+import { REST, Routes } from "discord.js";
 import { ExtendedClient } from "./structures/ExtendedClient.js";
 import { config } from "./config.js";
 import { db } from "./utils/database.js";
@@ -7,7 +7,7 @@ import { logger } from "./utils/logger.js";
 const deploy = async () => {
   const isGlobal = process.argv.includes('--global');
 
-  const client = new ExtendedClient({ intents: [GatewayIntentBits.Guilds] });
+  const client = new ExtendedClient({ intents: [] });
   await client.loadCommands();
 
   const rest = new REST({ version: '10' }).setToken(config.token);
