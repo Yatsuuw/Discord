41d79d8c65444251ad78f7cb4fe50d6c8c1eff4f
Yatsuu <pieters.lucas87@gmail.com>
2026-05-02 20:10:06 +0200

    Ajout du setPresence avec un rafraichissement automatique

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,13 +1,28 @@
-import { Events } from "discord.js";
+import { ActivityType, Events } from "discord.js";
 import type { Event } from "../../types/index.js";
 import { logger } from "../../utils/logger.js";
+
+const PRESENCE_REFRESH_MS = 4 * 60 * 100;
+
+function setPresence(client: import('discord.js').Client): void {
+  client.user?.setPresence({
+    activities: [{
+      name: 'Regarde Laid Back Camp saison 4',
+      type: ActivityType.Watching,
+    }],
+    status: 'online',
+  });
+}
 
 const event: Event<Events.ClientReady> = {
   name: Events.ClientReady,
   once: true,
-  execute(_client, c) {
+  execute(client, c): void {
     logger.info(`${c.user.tag} est connecté.`);
-  }
+
+    setPresence(client);
+    setInterval(() => setPresence(client), PRESENCE_REFRESH_MS);
+  },
 };
 
 export default event;
