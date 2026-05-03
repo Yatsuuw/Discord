967e124dd8eb44d5b1032f225418e07fc0f146d6
Yatsuu <pieters.lucas87@gmail.com>
2026-05-03 22:04:22 +0200

    Compilation de la dernière modification

--- dist/events/interactions/interactionCreate.js
+++ dist/events/interactions/interactionCreate.js
@@ -7,11 +7,12 @@ const event = {
     async execute(client, interaction) {
         if (!interaction.isChatInputCommand())
             return;
+        const iconURL = interaction.client.user.displayAvatarURL({ size: 32 });
         const command = client.commands.get(interaction.commandName);
         if (!command) {
             logger.warn(`Commande inconnue invoquée : /${interaction.commandName} par ${interaction.user.username}`);
             await interaction.reply({
-                embeds: [Templates.error('Cette commande n\'existe pas ou a été désactivée.', undefined, interaction.client.user.displayAvatarURL({ size: 32 }))],
+                embeds: [Templates.error('Cette commande n\'existe pas ou a été désactivée.', undefined, iconURL)],
                 flags: MessageFlags.Ephemeral
             });
             return;
@@ -21,7 +22,7 @@ const event = {
         }
         catch (error) {
             logger.error(`Erreur lors de l'exécution de /${interaction.commandName}`, error);
-            const errorEmbed = Templates.error(`Une erreur interne empêche l'exécution de cette commande.`, undefined, interaction.client.user.displayAvatarURL({ size: 32 }));
+            const errorEmbed = Templates.error(`Une erreur interne empêche l'exécution de cette commande.`, undefined, iconURL);
             if (interaction.replied || interaction.deferred) {
                 await interaction.followUp({ embeds: [errorEmbed], flags: MessageFlags.Ephemeral }).catch(() => { });
             }
