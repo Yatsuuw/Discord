7176123b03cd045479ebe6ac091236a52b9a4ab6
Yatsuu <pieters.lucas87@gmail.com>
2026-05-04 02:35:12 +0200

    setPresence déplacée en closure locale et readyClient utilisé cohéremment

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,30 +1,30 @@
-import { ActivityType, Events } from "discord.js";
+import { ActivityType, Client, Events } from "discord.js";
 import type { Event } from "../../types/index.js";
 import { logger } from "../../utils/logger.js";
 
 const PRESENCE_REFRESH_MS = 4 * 60 * 1_000;
 let presenceInterval: ReturnType<typeof setInterval> | null = null;
 
-function setPresence(client: import('discord.js').Client): void {
-  client.user?.setPresence({
-    activities: [{
-      name: 'Regarde Laid Back Camp saison 3',
-      type: ActivityType.Watching,
-    }],
-    status: 'online',
-  });
-}
-
 const event: Event<Events.ClientReady> = {
   name: Events.ClientReady,
   once: true,
-  execute(client, c): void {
-    logger.info(`${c.user.tag} est connecté.`);
+  execute(_client, readyClient: Client<true>): void {
+    logger.info(`${readyClient.user.tag} est connecté.`);
 
-    if (presenceInterval !== null) clearInterval(presenceInterval);
+    if (presenceInterval !== null) {
+      clearInterval(presenceInterval);
+      presenceInterval = null;
+    }
 
-    setPresence(client);
-    presenceInterval = setInterval(() => setPresence(client), PRESENCE_REFRESH_MS);
+    const setPresence = (): void => {
+      readyClient.user.setPresence({
+        activities: [{ name: 'Regarde Laid Back Camp saison 3', type: ActivityType.Watching }],
+        status: 'online',
+      });
+    };
+
+    setPresence();
+    presenceInterval = setInterval(setPresence, PRESENCE_REFRESH_MS);
   },
 };
 
