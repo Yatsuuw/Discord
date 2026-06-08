f2cfaac6c61a217cbfa542c661c00fbf94cf4163
Yatsuu <pieters.lucas87@gmail.com>
2026-06-08 17:40:46 +0200

    Ajout de la vérification db_port dans la condition

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,14 +2,14 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/index.js";
 import { logger } from "./logger.js";
 
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+if (!process.env.DB_HOST || !process.env.DB_PORT || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
   logger.error('❌ Configuration de la base de données incomplète. Vérifiez vos variables DB_HOST, DB_USER, DB_PASSWORD, DB_PORT et DB_NAME dans le fichier .env.');
   process.exit(1);
 }
 
 const adapter = new PrismaMariaDb({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
